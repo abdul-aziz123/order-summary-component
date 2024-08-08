0ef862d5ce30de7077117f37a650724aa027037e
abdul-aziz123 <k224933@nu.edu.pk>
2024-08-08 08:26:48 +0500

    first correction

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      "Pale-blue": " #e0e8ff",
+      "Bright-blue": "#3829e0",
+      "Very-pale-blue": "#f5f7ff",
+      "Desaturated-blue": "#7280a7",
+      "Dark-blue": "#1f2f56",
+    },
     extend: {},
   },
   plugins: [],

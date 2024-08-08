681a1a9c5d7522be28a783dd752b7b19b1ca6b83
Abdul Aziz <k224933@nu.edu.pk>
2024-08-08 20:14:13 +0500

    chore: Update favicon path and improve order summary layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      "Pale-blue": "#e0e8ff",
+      "Bright-blue": "#3829e0",
+      "Very-pale-blue": "#f5f7ff",
+      "Desaturated-blue": "#7280a7",
+      "Dark-blue": "#1f2f56",
+      white: "#ffffff",
+    },
+    extend: {
+      fontFamily: {
+        "Red-hat": ["Red Hat Display", "sans-serif"],
+      },
+      backgroundImage: {
+        "mobile-pattern": "url('images/pattern-background-mobile.svg')",
+        "desktop-pattern": "url('images/pattern-background-desktop.svg')",
+      },
+    },
   },
   plugins: [],
 };
